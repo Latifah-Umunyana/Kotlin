00628e5b54d372555285343a9c757f29d600767b
latifa <latifahumunyana@gmail.com>
2024-02-08 17:10:20 +0300

    add files

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var name = "Latifa Umunyana"
+var age = 20
+var phone = +250786230026
+var weight = 52
+var citizen: Boolean = false
+    println(name)
+    println(age)
+    println(phone)
+    println(weight)
+    println(citizen)
+
+}
